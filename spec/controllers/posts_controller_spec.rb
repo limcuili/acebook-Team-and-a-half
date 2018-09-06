55fedfde349495573111adae45b69214b59de494
Farah Jabri <farahjabri@hotmail.com>
2018-09-06 17:23:34 +0100

    all tests passing for posts controller, no tests for update action yet

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -43,10 +43,44 @@ RSpec.describe PostsController, type: :controller do
   end
 
   describe 'saving a post' do
-    it 'saves and redirects to posts_url' do
+    it 'saves post if params are complete' do
       expect(subject.save).to equal(true)
     end
   end
 
+  describe 'saving a post' do
+    it 'saves post if params are complete' do
+      subject = Post.new(user_name: 'Mo')
+      expect(subject.save).to equal(false)
+    end
+  end
+  describe 'not saving a post' do
+    it 'renders :new if post is missing params' do
+      subject = Post.new(user_name: 'Mo')
+      subject.save
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'saving a post' do
+    it 'redirects to posts_url if correct params' do
+      subject.save
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe '#destroy' do
+    it 'destroys the post correctly' do
+      subject.destroy
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe '#destroy' do
+    it 'updates the post correctly' do
+      subject = Post.new(user_name: 'Mo')
+      expect(subject.destroy).to_not have_http_status(200)
+    end
+  end
 
 end
