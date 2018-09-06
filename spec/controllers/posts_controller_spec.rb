5b19d7f5e5424b6fe6c9842997e41dbc845e9226
Farah Jabri <farahjabri@hotmail.com>
2018-09-06 16:47:12 +0100

    refactoring and DRY user controller spec, two more passing tests for posts controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,52 +3,50 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-  subject { Post.new }
+  subject { Post.new(user_name: 'Mo', message: "Hello World") }
 
-  describe '#new' do
-    it 'will not create a post if missing a message' do
+  describe 'Post with missing params' do
+    it 'has missing a message' do
+      subject.message = nil
+      subject.user_name = 'Mo'
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe 'Post with missing params' do
+    it 'has missing a user_name' do
+      subject.user_name = nil
+      subject.message = 'hello'
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe 'Post with missing params' do
+    it 'has missing a user_name and message' do
+      subject.user_name = nil
       subject.message = nil
       expect(subject).to_not be_valid
     end
   end
 
-  describe '#new' do
-    it 'will not create a post if missing a user_name' do
-      subject.user_name = nil
-      expect(subject).to_not be_valid
-    end
-  end
-
-  describe '#new' do
+  describe 'Post with correct params' do
     it 'will not create a post if missing a user_name and message' do
-      subject.user_name = nil
-      subject.message = nil
-      expect(subject).to_not be_valid
+      expect(subject).to be_valid
     end
   end
-      # post = Post.new(message: "Hello World", user_name: "Mo")
-  # describe 'POST /post' do
-  #   it 'post is saved when created' do
-  #
-  #   end
-  # end
-#
-#   describe 'POST /' do
-#     it 'responds with 200' do
-#       post :create, params: { post: { message: 'Hello, world!' } }
-#       expect(response).to redirect_to(posts_url)
-#     end
-#
-#     it 'creates a post' do
-#       post :create, params: { post: { message: 'Hello, world!' } }
-#       expect(Post.find_by(message: 'Hello, world!')).to be
-#     end
-#   end
-#
-#   describe 'GET /' do
-#     it 'responds with 200' do
-#       get :index
-#       expect(response).to have_http_status(200)
-#     end
-#   end
+
+  describe 'saving a post' do
+    it 'saves and redirects to posts_url' do
+      subject.save
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'saving a post' do
+    it 'saves and redirects to posts_url' do
+      expect(subject.save).to equal(true)
+    end
+  end
+
+
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-
+  subject { User.new(name: 'Zara', email: 'zarah@example.com', password: '12345') }
   describe 'Get /users' do
     it 'renders the users/index page' do
       get :index
@@ -18,16 +18,14 @@ RSpec.describe UsersController, type: :controller do
 
   describe 'POST /users' do
     it 'User is saved when created' do
-      user = User.new(name: 'Zara', email: 'zarah@example.com', password: '12345')
-      expect(user.save).to equal(true)
+      expect(subject.save).to equal(true)
       expect(response).to have_http_status(200)
     end
   end
 
   describe 'POST /users' do
     it 'Two identical users can not be created' do
-      user = User.new(name: 'Zara', email: 'zarah@example.com', password: '12345')
-      expect(user.save).to equal(true)
+      expect(subject.save).to equal(true)
       user2 = User.new(name: 'Zara', email: 'zarah@example.com', password: '12345')
       expect(user2.save).to equal(false)
       expect(response).to have_http_status(200)
@@ -36,16 +34,16 @@ RSpec.describe UsersController, type: :controller do
 
   describe 'POST /users' do
     it 'Incomplete user params will sucsessfully redirect to /login' do
-      user2 = User.new(email: 'zarah@example.com', password: '12345')
-      expect(user2.save).to equal(false)
+      subject = User.new(email: 'zarah@example.com', password: '12345')
+      expect(subject.save).to equal(false)
       expect(response).to have_http_status(200)
     end
   end
 
   describe 'POST /users' do
     it 'User params missing will not save user and redirect to /login' do
-      user = User.new
-      expect(user.save).to equal(false)
+      subject = User.new(email: 'zarah@example.com', password: '12345')
+      expect(subject.save).to equal(false)
     end
   end
 
