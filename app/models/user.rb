c4693c2480d42af72a8963a827e4ed047256c56e
Farah Jabri <farahjabri@hotmail.com>
2018-09-06 12:17:56 +0100

    passing unit tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, uniqueness: true
+  validates :email, uniqueness: true
+  validates_presence_of :password
 
   has_one_attached :avatar
 end
